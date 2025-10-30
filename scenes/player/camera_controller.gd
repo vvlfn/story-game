f00692c8caf26f207ff3db707dfe03c6d0836f2b
vvlfn <szkolapawel16@gmail.com>
2025-10-30 19:43:58 +0100

    .

--- scenes/player/camera_controller.gd
+++ scenes/player/camera_controller.gd
@@ -1,20 +1,13 @@
 extends Node3D
 
 @onready var root: CharacterBody3D = get_parent()
-@onready var camera: Camera3D = root._camera
-@onready var cameraPos = $CameraPos
-
-var SENS: Vector2 = Vector2(75, 75)
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(_delta: float) -> void:
-	camera.global_position = cameraPos.global_position
-	camera.look_at(global_position)
-	
-	var cameraInput: Vector2 = Input.get_vector("look_left", "look_right", "look_up", "look_down")
-	rotation_degrees.y += cameraInput.x * SENS.x / 50
-	rotation_degrees.x += cameraInput.y * SENS.y / 50
-	print(rotation_degrees.x)
-	#rotation_degrees.x = clampf(rotation_degrees.x, -15, -80)
+	var turnInput: Vector2 = Input.get_vector("look_left","look_right", "look_up","look_down")
+	root.rotation.y -= turnInput.x / 25
+	var newCameraPos: float = $CameraPos.position.y - turnInput.y / 25
+	$CameraPos.position.y = clampf(newCameraPos, 0, 3) 
+		
--- scenes/player/movement_controller.gd
+++ scenes/player/movement_controller.gd
@@ -3,20 +3,37 @@ extends CharacterBody3D
 @onready var animation = $AnimationPlayer
 @export var _camera: Camera3D
 
+var JUMP_STRENGTH: float = 300.0
+
+
 var SPEED: float = 2.0
 func _ready() -> void:
 	pass
 	
-func _process(_delta: float) -> void:
-	var movementInput: Vector2 = Input.get_vector("walk_left", "walk_right", "walk_forward", "walk_back")
-	#var headingDirection: Vector3 = (transform.basis.z)
-	var direction: Vector3 = (Vector3(movementInput.x, 0, movementInput.y)).normalized()
-	velocity = direction * SPEED
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity += get_gravity()
+	
+	_camera.global_position = $CameraCenter/CameraPos.global_position
+	_camera.look_at($CameraCenter.global_position)
+	
+	var movementInput: Vector2 = Input.get_vector("walk_left", "walk_right","walk_forward", "walk_back")
+	var moveDirection: Vector3 = (transform.basis * Vector3(movementInput.x, 0, movementInput.y)).normalized()
+	#var turnDirection: float = movementInput.x
+	
+	velocity = moveDirection * SPEED
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		print("test")
+		velocity.y += JUMP_STRENGTH
+	
+	#rotation_degrees.y -= turnDirection * 3
 	if velocity:
 		animation.play("walk")
-		rotation.y = atan2(-direction.x, -direction.z)
+		#rotation.y = atan2(-direction.x, -dire-ction.z)
 	else:
 		animation.stop()
+		
 	
 	move_and_slide()
 	
