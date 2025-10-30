11b4c0ffb6365c52766bbf042ac3225dde6a8cbe
vvlfn <szkolapawel16@gmail.com>
2025-10-30 23:57:24 +0100

    some stuff i tihnk:

--- scenes/player/camera_controller.gd
+++ scenes/player/camera_controller.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @onready var root: CharacterBody3D = get_parent()
-var SENSITIVITY: Vector2 = Vector2(3, 3)
+var SENSITIVITY: Vector2 = Vector2(2, 2)
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -9,6 +9,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	var turnInput: Vector2 = Input.get_vector("look_left","look_right", "look_up","look_down")
 	root.rotation.y -= turnInput.x / 100 * SENSITIVITY.x
-	var newCameraPos: float = $CameraPos.position.y + (turnInput.y / 100 * SENSITIVITY.y)
+	var newCameraPos: float = $CameraPos.position.y - (turnInput.y / 100 * SENSITIVITY.y)
 	$CameraPos.position.y = clampf(newCameraPos, 0, 3) 
 		
--- scenes/player/movement_controller.gd
+++ scenes/player/movement_controller.gd
@@ -3,16 +3,14 @@ extends CharacterBody3D
 @onready var animation = $AnimationPlayer
 @export var _camera: Camera3D
 
-var JUMP_STRENGTH: float = 300.0
-
-
-var SPEED: float = 2.0
+var JUMP_STRENGTH: float = 5.0
+var SPEED: float = 5.0
 func _ready() -> void:
 	pass
 	
-func _physics_process(_delta: float) -> void:
-	if !is_on_floor():
-		velocity += get_gravity()
+func _process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	
 	_camera.global_position = $CameraCenter/CameraPos.global_position
 	_camera.look_at($CameraCenter.global_position)
@@ -21,19 +19,23 @@ func _physics_process(_delta: float) -> void:
 	var moveDirection: Vector3 = (transform.basis * Vector3(movementInput.x, 0, movementInput.y)).normalized()
 	#var turnDirection: float = movementInput.x
 	
-	velocity = moveDirection * SPEED
 	
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if moveDirection:
+		velocity.x = moveDirection.x * SPEED * delta * 60
+		velocity.z = moveDirection.z * SPEED * delta * 60
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		print("test")
-		velocity.y += JUMP_STRENGTH
+		velocity.y = JUMP_STRENGTH
 	
-	#rotation_degrees.y -= turnDirection * 3
-	if velocity:
+	
+	if velocity and is_on_floor():
 		animation.play("walk")
-		#rotation.y = atan2(-direction.x, -dire-ction.z)
 	else:
 		animation.stop()
 		
-	
+	print(is_on_floor())
 	move_and_slide()
-	
